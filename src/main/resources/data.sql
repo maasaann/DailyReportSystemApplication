3049c81387c70f1627b35cfc68fc042be0b0a967
yoshitoshi.​miyata <meyutana@gmail.com>
2024-01-31 00:17:32 +0900

    2024年1月31日午前0時17分

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,3 @@ INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,
      VALUES (CURRENT_TIMESTAMP,"煌木　太郎の記載、タイトル","煌木　太郎の記載、内容",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
      VALUES (CURRENT_TIMESTAMP,"田中　太郎の記載、タイトル","田中　太郎の記載、内容",2,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-;
